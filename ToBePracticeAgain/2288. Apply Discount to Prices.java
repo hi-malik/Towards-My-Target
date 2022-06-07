bf3899a8b7a173a574aee63c102eb19f4fae4ec5
Himanshu Malik <jsm.28415@gmail.com>
2022-06-07 22:21:40 +0530

    Day `0`

---
+++ ToBePracticeAgain/2288. Apply Discount to Prices.java
@@ -0,0 +1,33 @@
+// Better
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+class Solution {
+    public String discountPrices(String sentence, int discount) {
+        String str[] = sentence.split(" ");
+        for(int i = 0; i < str.length; i++){
+            if(str[i].charAt(0) != '$' || str[i].length() < 2) continue;
+            
+            long ans = 0;
+            boolean flag = true;
+            
+            for(int j = 1; j < str[i].length(); j++){
+                char ch = str[i].charAt(j);
+                if(Character.isDigit(ch)){
+                    ans *= 10;
+                    ans += ch - '0';
+                }
+                else {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag){
+                double descount = ans * 0.01 * discount;
+                descount = (double)ans - descount;
+                str[i] = "$" + String.format("%.2f", descount); 
+            }
+        }
+        return String.join(" ", str);
+    }
+}
