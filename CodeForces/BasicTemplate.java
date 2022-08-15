5bf5e5f08436af1eaeb4bb81604014ec676354f8
Himanshu Malik (malikh11) <malikh11@coventry.ac.uk>
2022-08-15 12:00:37 +0530

    Java-Basic_TemplateCodeSnippet

---
+++ CodeForces/BasicTemplate.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class BasicTemplate{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        
+        System.out.println("Print Here");
+    }
+}
