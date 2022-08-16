8e6160f49203068da4281ec31197baa99fedd0d4
Himanshu Malik (malikh11) <malikh11@coventry.ac.uk>
2022-08-16 21:38:55 +0530

    CodeForces problem 150 A level

---
+++ CodeForces/problem50A.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class problem50A{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+
+        int sum = (m * n) / 2;
+        System.out.println(sum);
+        // System.out.println("Print Here");
+    }
+}
+// Problem link :- https://codeforces.com/contest/50/problem/A
