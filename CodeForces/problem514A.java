a31072c2eb25475e711ada5f266ff8accd65bfe6
Himanshu Malik (malikh11) <malikh11@coventry.ac.uk>
2022-08-26 13:40:38 +0530

    A level - solved using 2 diff solns

--- CodeForces/problem514A.java
+++ CodeForces/problem514A.java
@@ -1,22 +1,36 @@
 import java.util.*;
 public class problem514A{
     public static void main(String[] args){
+        // Solution - 1
+        /* 
         Scanner scn = new Scanner(System.in);
         String s = scn.nextLine();
-        StringBuilder sb = new StringBuilder();
+        String ans = new String();
 
-        int a = s.charAt(0) - '0';
-        if(a == 9) sb.append('9');
-        else if(a > 4) sb.append((9 - a) + '0');
-        // else sb.append(a + '0');
-
-        for(int i = 1; i < s.length(); i++){
-            int b = s.charAt(i) - '0';
-            // if(b == 0 || b < 5) sb.append(b + '0');
-            if(b == 0 || b < 5) continue;
-            else sb.append((9 - b) + '0');
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(0) == '9' && i == 0) ans += s.charAt(0);
+            else{
+                int a = s.charAt(i) - '0';
+                int b = 9 - a;
+                ans += Math.min(a, b);
+            }
         }
-        System.out.println(sb.toString());
+        System.out.println(ans);
+        */
+        // Solution - 2
+        Scanner scn = new Scanner(System.in);
+        long n = scn.nextLong(), ans = 0, t = 1;
+
+        while(n > 0){
+            if(n == 9){
+                ans += 9 * t;
+            }
+            long temp = n % 10;
+            ans += Math.min(temp, 9 - temp) * t;
+            t *= 10;
+            n /= 10;
+        }
+        System.out.println(ans);
     }
 }
 // Problem Link :- 
