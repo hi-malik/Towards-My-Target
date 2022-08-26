1c810e774beceb1e01b96a144334264556c6d685
Himanshu Malik (malikh11) <malikh11@coventry.ac.uk>
2022-08-26 11:42:18 +0530

    A level

---
+++ CodeForces/problem148A.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class problem148A{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int k = scn.nextInt();
+        int l = scn.nextInt();
+        int m = scn.nextInt();
+        int n = scn.nextInt();
+        int d = scn.nextInt();
+
+        int count = 0;
+        for(int i = 1; i <= d; i++){
+            if(i % k == 0 || i % l == 0 || i % m == 0 || i % n == 0) count++;
+        }
+        System.out.println(count);
+        // System.out.println("Print Here");
+    }
+}
+// Problem Link :- 
