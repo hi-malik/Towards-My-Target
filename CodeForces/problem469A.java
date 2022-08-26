7075da9dda1b502ff07fb13e2f56a0c059f8750f
Himanshu Malik (malikh11) <malikh11@coventry.ac.uk>
2022-08-26 09:05:07 +0530

    A level

--- CodeForces/problem469A.java
+++ CodeForces/problem469A.java
@@ -4,31 +4,28 @@ public class problem469A{
         Scanner scn = new Scanner(System.in);
         
         int n = scn.nextInt();
-        String s1 = scn.nextLine();
-        String s2 = scn.nextLine();
-
+        
+        int p = scn.nextInt();
+        int a1[] = new int[p];
+        int i = 0, k = 0;
+        while(p --> 0){
+            a1[i++] = scn.nextInt();
+        }
+        int q = scn.nextInt();
+        int a2[] = new int[q];
+        while(q --> 0){
+            a2[k++] = scn.nextInt();
+        }
         Set<Integer> set = new HashSet<>();
-        Set<Integer> set2 = new HashSet<>();
+        for(int s : a1) set.add(s);
+        for(int s : a2) set.add(s);
 
-        for(int i = 0; i < s1.length(); i++) {
-            char ch = s1.charAt(i);
-            int a = ch - '0';
-            set.add(a);
-        }
-        for(int i = 0; i < s2.length(); i++) {
-            char ch = s2.charAt(i);
-            int a = ch - '0';
-            set.add(a);
-        }
-        for(int i = 1; i < n; i++){
-            if(!set.contains(i)){
-                System.out.println("Oh, my keyboard!"); 
-                return;
+        
+            if(set.size() != n){
+                System.out.println("Oh, my keyboard!");
             }
-        }
-        System.out.println("I become the guy.");
-        // if(set.contains(n) || set2.contains(n)) System.out.println("I become the guy.");
-        // else System.out.println("Oh, my keyboard!"); 
+        else 
+            System.out.println("I become the guy.");
 
         // System.out.println("Print Here");
     }
