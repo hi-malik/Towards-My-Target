f7685b6b942c7f9da2b9b45aba199ef2fccd793f
Himanshu Malik (malikh11) <malikh11@coventry.ac.uk>
2022-08-29 18:39:12 +0530

    A level

---
+++ CodeForces/problem1303A.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class problem1303A{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        while(n --> 0){
+            int count = 0;
+            String s = scn.next();
+
+            for(int i = s.indexOf('1'); i < s.lastIndexOf('1'); i++){
+                if(s.charAt(i) == '0') count++;
+            }
+            System.out.println(count);
+        }
+    }
+}
+// Problem Link :- 
