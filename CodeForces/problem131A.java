148d58f844d75e9e7b77038cae26ea2326bca593
Himanshu Malik (malikh11) <malikh11@coventry.ac.uk>
2022-09-01 23:00:21 +0530

    A level

--- CodeForces/problem131A.java
+++ CodeForces/problem131A.java
@@ -1,41 +1,15 @@
 import java.util.*;
 public class problem131A{
     public static void main(String[] args){
-        Scanner scn = new Scanner(System.in);
-        String s = scn.nextLine();
-
-        System.out.println(convert(s));
-    }
-    public static String convert(String str)
-    {
- 
-        // Create a char array of given String
-        char ch[] = str.toCharArray();
-        for (int i = 0; i < str.length(); i++) {
- 
-            // If first character of a word is found
-            if (i == 0 && ch[i] != ' ' ||
-                ch[i] != ' ' && ch[i - 1] == ' ') {
- 
-                // If it is in lower-case
-                if (ch[i] >= 'a' && ch[i] <= 'z') {
- 
-                    // Convert into Upper-case
-                    ch[i] = (char)(ch[i] - 'a' + 'A');
-                }
-            }
- 
-            // If apart from first character
-            // Any one is in Upper-case
-            else if (ch[i] >= 'A' && ch[i] <= 'Z')
- 
-                // Convert into Lower-Case
-                ch[i] = (char)(ch[i] + 'a' - 'A');           
-        }
- 
-        // Convert the char array to equivalent String
-        String st = new String(ch);
-        return st;
+        Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		if(s.matches("^[a-z]?[A-Z]*$")){
+			String ans = s.toLowerCase();
+			if(s.charAt(0) >= 'a' && s.charAt(0) <= 'z')
+				ans = ans.substring(0,1).toUpperCase() + ans.substring(1);
+			s = ans;
+		}
+		System.out.println(s);
     }
 }
 // Problem Link :- https://codeforces.com/problemset/problem/131/A
